6006042850e275de5c87312df7c4c5a974aea67f
Yurii <puradragonfire@gmail.com>
2026-03-14 19:23:36 +0200

    Р

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ document.getElementById("form").addEventListener("submit",function(e){
 
 e.preventDefault()
 
-let price=document.getElementById("category").value
-let email=document.getElementById("email").value
-let phone=document.getElementById("phone").value
 
-let url="https://secure.wayforpay.com/button/b1ad7b3132f11?&clientEmail="+email+"&clientPhone="+phone
+
+let url="https://secure.wayforpay.com/button/b1ad7b3132f11?
 
 window.location.href=url
 
